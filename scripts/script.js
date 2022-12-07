7aab5350977a79acf227ff6a0a3b537687d874a4
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-07 14:50:11 +0100

    logboek uitklap knop animatie
    vet mooi met javascript! :)

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let active = document.querySelector('.active');
+// active.addEventListener('click', opacity1)
+
+
+function opacity1(){
+    active.style.height = '700px';
+    active.style.opacity='1';
+}
+
