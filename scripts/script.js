95ed09673c869720c26c10157292b0ff68ed4abd
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-08 16:07:39 +0100

    logboek mooie microinteractie
    toggle gebruikt

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
-let active = document.querySelector('.active');
-// active.addEventListener('click', opacity1)
+let button = document.querySelector('.logbook button')
+let weekOverview = document.querySelector('.logbook div');
+button.addEventListener('click', openLogboek)
 
 
-function opacity1(){
-    active.style.height = '700px';
-    active.style.opacity='1';
+function openLogboek(){
+    // active.style.height = '700px';
+    // active.style.opacity='1';
+    // weekOverview.classList.toggle('pipo');
+    weekOverview.classList.toggle('active');
+    // weekOverview.classList.replace('pipo','pipa')
+    
 }
+
+
 
