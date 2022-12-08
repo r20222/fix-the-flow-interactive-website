7d619445e07753c7da9788459c1ae8d919eab32c
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-08 21:33:51 +0100

    logboek buttons tab

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,22 @@
-let button = document.querySelector('.logbook button')
+let button = document.querySelector('.logbook button');
 let weekOverview = document.querySelector('.logbook div');
-button.addEventListener('click', openLogboek)
+let tabButton = document.querySelector('.logbook li button');
+// let tabButton = document.getElementById('tabbutton');
+button.addEventListener('click', openLogboek);
 
 
 function openLogboek(){
     weekOverview.classList.toggle('active');
-}
 
+    if(tabButton.tabIndex == "-1"){
+        tabButton.tabIndex="0";
+        // alert('-1')
+    }
+    else {
+        tabButton.tabIndex="-1";
+        // alert('0')
+    }
+    
+}
 
 
