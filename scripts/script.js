f3a313a1c2b26185e5c0c3746e84e094c579797a
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-09 10:24:30 +0100

    alle buttons in logboek op tab

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,23 @@
 let button = document.querySelector('.logbook button');
 let weekOverview = document.querySelector('.logbook div');
-let tabButton = document.querySelector('.logbook li button');
+let tabButtons = document.querySelectorAll('.logbook li button');
 // let tabButton = document.getElementById('tabbutton');
 button.addEventListener('click', openLogboek);
 
 
 function openLogboek(){
     weekOverview.classList.toggle('active');
-
-    if(tabButton.tabIndex == "-1"){
-        tabButton.tabIndex="0";
-        // alert('-1')
-    }
-    else {
-        tabButton.tabIndex="-1";
-        // alert('0')
-    }
+    tabButtons.forEach(function(tabButton) {
+        if(tabButton.tabIndex == "-1"){
+            tabButton.tabIndex="0";
+            // alert('-1')
+        }
+        else {
+            tabButton.tabIndex="-1";
+            // alert('0')
+        }
+    })
+    
     
 }
 
