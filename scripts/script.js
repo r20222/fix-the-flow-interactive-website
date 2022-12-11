17503ddcf1c1558af2afa2102ae0ef5adfda2e25
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-11 14:17:11 +0100

    classes and focus
    added some classes and a tab focus on the info buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
-let button = document.querySelector('.logbook button');
-let weekOverview = document.querySelector('.logbook div');
-let tabButtons = document.querySelectorAll('.logbook li button');
+let button = document.querySelector('.button-accordion');
+let weekOverview = document.querySelector('.expand');
+let tabButtons = document.querySelectorAll('.button-day');
 
 
 button.addEventListener('click', openLogbook);
 
 
 function openLogbook(){
+    
     weekOverview.classList.toggle('active');
+
     tabButtons.forEach(function(tabButton) {
         if(tabButton.tabIndex == "-1"){
             tabButton.tabIndex="0";
