bccb9f407eb9505f7e517e23f4ca5fab3825bd2e
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-11 11:47:41 +0100

    english
    and i deleted unnecesary CSS values.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,19 @@
 let button = document.querySelector('.logbook button');
 let weekOverview = document.querySelector('.logbook div');
 let tabButtons = document.querySelectorAll('.logbook li button');
-// let tabButton = document.getElementById('tabbutton');
-button.addEventListener('click', openLogboek);
 
 
-function openLogboek(){
+button.addEventListener('click', openLogbook);
+
+
+function openLogbook(){
     weekOverview.classList.toggle('active');
     tabButtons.forEach(function(tabButton) {
         if(tabButton.tabIndex == "-1"){
             tabButton.tabIndex="0";
-            // alert('-1')
         }
         else {
             tabButton.tabIndex="-1";
-            // alert('0')
         }
     })
     
