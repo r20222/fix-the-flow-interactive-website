0edf6e788e9e43dcb59cc0e4620fc854f065bf1f
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-19 22:15:28 +0100

    arrow up JS
    het klopt nog niet helemaal want alle pijlen keren nu om, maar ik wil alleen dat de pijl omdraait die ik specifiek heb aangeklikt

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,39 @@ buttons.forEach(button => {
     button.addEventListener('click', openLogbookSection, false);
 })
 
+// arrow up and down on click 
+
+let arrowDown = document.querySelectorAll('.arrow-down')
+
+
+
 function openLogbookSection(event){
+
     console.log(event) 
     const panel = event.target.nextElementSibling;
-    
     panel.classList.toggle('active');
+
+    // let img = event.target.srcElement.firstElementChild.attributes.src;
+    // console.log(img);
+   
+    // if (img.src.match("assets/arrow-down.svg")) {
+    //     img.src = "assets/arrow-up.svg";
+    //     }
+    // else if(img.src.match("assets/arrow-up.svg")) {
+    //         img.src = "assets/arrow-down.svg";
+    //     }
+    
+
+    arrowDown.forEach(down =>{
+        if (down.src.match("assets/arrow-down.svg")) {
+            down.src = "assets/arrow-up.svg";
+        }
+        else if(down.src.match("assets/arrow-up.svg")) {
+            down.src = "assets/arrow-down.svg";
+        }
+    })
+    
+    
 
     tabButtons.forEach(function(tabButton) {
         if(tabButton.tabIndex == "-1"){
