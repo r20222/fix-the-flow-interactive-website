59974ee5417b9851d775fd5573802c8696023411
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-21 11:08:18 +0100

    rem

--- scripts/script.js
+++ scripts/script.js
@@ -9,27 +9,35 @@ buttons.forEach(button => {
 
 // arrow up and down on click 
 
-let arrowDown = document.querySelectorAll('.arrow-down')
-
-
 
 function openLogbookSection(event){
 
     console.log(event) 
     const panel = event.target.nextElementSibling;
     panel.classList.toggle('active');
+    // const probeersel = event.target;
 
-    // let img = event.target.srcElement.firstElementChild;
-    // console.log(img);
-   
-    // if (img.src.match("assets/arrow-down.svg")) {
-    //     img.src = "assets/arrow-up.svg";
-    //     }
-    // else if(img.src.match("assets/arrow-up.svg")) {
-    //         img.src = "assets/arrow-down.svg";
-    //     }
+    // hieronder probeersel
+// let button1 = document.getElementById('#example');
+// let arrowDown1 = document.querySelector('.arrow-down1')
+
+
+// console.log(panel.classList.contains('active'));
+// if(panel.classList.contains('active')){
+//     arrowDown1.src = "assets/arrow-up.svg";
+// } else {
+//     arrowDown1.src = "assets/arrow-down.svg";
+// }
+
+
+
+
+
+
+
+    // hieronder een werkende code voor de 3 buttons tegelijk
+    let arrowDown = document.querySelectorAll('.arrow-down')
     
-
     arrowDown.forEach(down =>{
         if (down.src.match("assets/arrow-down.svg")) {
             down.src = "assets/arrow-up.svg";
@@ -38,7 +46,6 @@ function openLogbookSection(event){
             down.src = "assets/arrow-down.svg";
         }
     })
-    
     
 
     tabButtons.forEach(function(tabButton) {
